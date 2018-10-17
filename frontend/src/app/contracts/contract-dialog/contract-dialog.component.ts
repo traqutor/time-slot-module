267bbcc76c0d2fdd45cc54fed9df8b90d5a27826
Mariusz Augustyniak <maugustyniak@gmail.com>
2018-10-17 10:59:35 +0200

    Contract properties vendor supplier commodity

--- frontend/src/app/contracts/contract-dialog/contract-dialog.component.ts
+++ frontend/src/app/contracts/contract-dialog/contract-dialog.component.ts
@@ -2,6 +2,12 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
 import {IContract} from "../contract.model";
+import {VendorService} from "../../vendors/vendor.service";
+import {SupplierService} from "../../suppliers/supplier.service";
+import {CommodityService} from "../../commodities/commodity.service";
+import {IVendor} from "../../vendors/vendor.model";
+import {ISupplier} from "../../suppliers/supplier.model";
+import {ICommodity} from "../../commodities/commodity.model";
 
 @Component({
   selector: 'app-contract-dialog',
@@ -11,9 +17,15 @@ import {IContract} from "../contract.model";
 export class ContractDialogComponent implements OnInit {
 
   contractForm: FormGroup;
+  vendors: IVendor[];
+  suppliers: ISupplier[];
+  commodities: ICommodity[];
 
   constructor(public dialogRef: MatDialogRef<ContractDialogComponent>,
               private formBuilder: FormBuilder,
+              private vendorService: VendorService,
+              private supplierService: SupplierService,
+              private commodityService: CommodityService,
               @Inject(MAT_DIALOG_DATA) public contract: IContract) {
   }
 
@@ -23,6 +35,9 @@ export class ContractDialogComponent implements OnInit {
     this.contractForm = this.formBuilder.group({
       id: this.contract.id,
       name: [this.contract.name, [Validators.required,]],
+      vendor: [this.contract.vendor, [Validators.required]],
+      supplier: [this.contract.supplier, [Validators.required]],
+      commodity: [this.contract.commodity, [Validators.required]],
       creationDate: this.contract.creationDate,
       modificationDate: this.contract.modificationDate,
       createdBy: this.contract.createdBy,
@@ -30,6 +45,26 @@ export class ContractDialogComponent implements OnInit {
       entityStatus: this.contract.entityStatus
     });
 
+    this.vendorService.getVendors();
+
+    this.vendorService.vendorsChanged.subscribe((res: IVendor[]) => {
+      this.vendors = res;
+    });
+
+    this.supplierService.getSuppliers();
+    this.supplierService.suppliersChanged.subscribe((res: ISupplier[]) => {
+      this.suppliers = res;
+    });
+
+    this.commodityService.getCommodities();
+    this.commodityService.commoditiesChanged.subscribe((res: ISupplier[]) => {
+      this.suppliers = res;
+    });
+
+  }
+
+  compare(val1, val2) {
+    return val1 && val2 ? val1.id === val2.id : val1 === val2;
   }
 
 
--- frontend/src/app/contracts/contract.model.ts
+++ frontend/src/app/contracts/contract.model.ts
@@ -1,8 +1,14 @@
 import {IUser} from "../users/user.model";
+import {IVendor} from "../vendors/vendor.model";
+import {ISupplier} from "../suppliers/supplier.model";
+import {ICommodity} from "../commodities/commodity.model";
 
 export interface IContract {
   id: number;
   name: string;
+  vendor: IVendor
+  supplier: ISupplier
+  commodity: ICommodity
   creationDate: Date;
   modificationDate: Date;
   createdBy: IUser;
